6d9beb15d6736030a4ffc4ccbcab53cb539b18c4
biwottson <biwottson007@gmail.com>
2023-06-15 14:43:16 +0000

    Hello World

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,6 +1,22 @@
+/*
+ * File: 101-quote.c
+ * Author: OpenAI
+ * Description: Prints a quote to the standard error
+ * Return: Always 1
+ */
+
 #include <unistd.h>
+
+/**
+ * main - Entry point of the program
+ *
+ * Description: Prints the quote "and that piece of art is useful" - Dora Korpar, 2015-10-19"
+ * to the standard error.
+ *
+ * Return: Always 1
+ */
 int main(void)
 {
 	write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
-    return 1;
+	return (1);
 }
