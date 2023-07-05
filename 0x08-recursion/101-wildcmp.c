1d942ac26dbcf6b5a26dc0e390aff9ebce527bc0
biwottson <biwottson007@gmail.com>
2023-07-05 20:58:36 +0000

    0x08. C - Recursion

--- 0x08-recursion/101-wildcmp.c
+++ 0x08-recursion/101-wildcmp.c
@@ -1,4 +1,15 @@
-#include "main.h"
+trlen_recursion(s + 1));
+}
+
+/**
+ *  * check_pal - checks the characters recursively for palindrome
+ *   * @s: string to check
+ *    * @i: iterator
+ *     * @len: length of the string
+ *      *
+ *       * Return: 1 if palindrome, 0 if not
+ *        */
+int chec#include "main.h"
 
 int check_pal(char *s, int i, int len);
 int _strlen_recursion(char *s);
@@ -24,20 +35,7 @@ int is_palindrome(char *s)
  */
 int _strlen_recursion(char *s)
 {
-	if (*s == '\0')
-		return (0);
-	return (1 + _strlen_recursion(s + 1));
-}
-
-/**
- * check_pal - checks the characters recursively for palindrome
- * @s: string to check
- * @i: iterator
- * @len: length of the string
- *
- * Return: 1 if palindrome, 0 if not
- */
-int check_pal(char *s, int i, int len)
+	if (*sk_pal(char *s, int i, int len)
 {
 	if (*(s + i) != *(s + len - 1))
 		return (0);
