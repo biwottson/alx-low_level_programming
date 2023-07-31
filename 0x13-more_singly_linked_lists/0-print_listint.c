551a4d9d8144b8940664e0e39f32885780f8dc0e
biwottson <biwottson007@gmail.com>
2023-07-31 15:56:00 -0700

    0x13. C - More singly linked lists Updatedgit add .

--- 0x13-more_singly_linked_lists/0-print_listint.c
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -12,10 +12,12 @@ size_t print_listint(const listint_t *h)
 
 	while (h)
 	{
-		printf("%d\n", h->n);
+		_putchar(h->n + '0');
+		_putchar('\n');
 		num++;
 		h = h->next;
 	}
 
 	return (num);
 }
+
