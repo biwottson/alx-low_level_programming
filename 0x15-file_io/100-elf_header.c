312efea67b0f5095b213952ee599de5f2ef2a369
biwottson <biwottson007@gmail.com>
2023-08-07 22:25:59 +0000

    0x15. C - File I/O

--- 0x15-file_io/100-elf_header.c
+++ 0x15-file_io/100-elf_header.c
@@ -4,7 +4,7 @@
 #include <unistd.h>
 #include <fcntl.h>
 
-void check_elf(unsigned char *e_ident);
+void check_elf(void);
 void print_magic(unsigned char *e_ident);
 void print_class(unsigned char *e_ident);
 void print_data(unsigned char *e_ident);
@@ -16,17 +16,32 @@ void print_entry(unsigned long int e_entry, unsigned char *e_ident);
 void close_elf(int elf);
 
 /**
- * check_elf - Checks if a file is an ELF file.
- * @e_ident: A pointer to an array containing the ELF magic numbers.
- */
-void check_elf(unsigned char *e_ident)
+ *  * check_elf - Checks if a file is an ELF file.
+ *   * @e_ident: A pointer to an array containing the ELF magic numbers.
+ *    */
+void check_elf(void)
 {
-	/* Function remains the same */
+		/* Function implementation */
 }
 
 /* Other functions remain unchanged */
 
-int main(int __attribute__((__unused__)) argc, char *argv[])
+int main(int _#include <elf.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <fcntl.h>
+
+void check_elf(void);
+void print_magic(unsigned char *e_ident);
+void print_class(unsigned char *e_ident);
+void print_data(unsigned char *e_ident);
+void print_version(unsigned char *e_ident);
+void print_abi(unsigned char *e_ident);
+void print_osabi(unsigned char *e_ident);
+void print_type(unsigned int e_type, unsigned char *e_ident);
+void print_entry(unsigned long int e_entry, unsigned char *e_ident);
+void c_attribute__((__unused__)) argc, char *argv[])
 {
 	Elf64_Ehdr *header;
 	int o, r;
@@ -53,7 +68,7 @@ int main(int __attribute__((__unused__)) argc, char *argv[])
 		exit(98);
 	}
 
-	check_elf(header->e_ident);
+	check_elf();
 	printf("ELF Header:\n");
 	print_magic(header->e_ident);
 	print_class(header->e_ident);
